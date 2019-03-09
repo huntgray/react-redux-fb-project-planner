a72e11b4a14b796a649e072ac912f435142b6e97
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2019-03-09 12:59:51 -0500

    adds support for newUser signup in firebase; creates new user in firestore with name and initial properties

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,50 +1,52 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
 
 class SignUp extends Component {
   state = {
     email: '',
     password: '',
     firstName: '',
-    lastName: ''
+    lastName: '',
   }
   handleChange = (e) => {
     this.setState({
-      [e.target.id]: [e.target.value]
-    });
+      [e.target.id]: e.target.value
+    })
   }
-
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signUp(this.state);
   }
-
-  render(){
-    const { auth } = this.props;
+  render() {
+    const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to='/' />
-    return(
+    return (
       <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-5">Sign Up</h5>
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
+            <input type="email" id='email' onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
+            <input type="password" id='password' onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" onChange={this.handleChange} />
+            <input type="text" id='firstName' onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" onChange={this.handleChange} />
+            <input type="text" id='lastName' onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+            <div className="center red-text">
+              { authError ? <p>{authError}</p> : null }
+            </div>
           </div>
         </form>
       </div>
@@ -54,8 +56,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
-export default connect(mapStateToProps)(SignUp)
+const mapDispatchToProps = (dispatch)=> {
+  return {
+    signUp: (creds) => dispatch(signUp(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -10,16 +10,34 @@ const authReducer = (state = initState, action) => {
         ...state,
         authError: 'Login failed'
       }
+
     case 'LOGIN_SUCCESS':
       console.log('login success');
       return {
+        ...state,
         authError: null
       }
+
     case 'SIGNOUT_SUCCESS':
       console.log('signout success');
-      return state
-    default:
       return state;
+
+    case 'SIGNUP_SUCCESS':
+      console.log('signup success')
+      return {
+        ...state,
+        authError: null
+      }
+
+    case 'SIGNUP_ERROR':
+      console.log('signup error')
+      return {
+        ...state,
+        authError: action.err.message
+      }
+
+    default:
+      return state
   }
 };
 
