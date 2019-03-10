f2165adb6ad7bdc84005c39664d4afc2ed70e334
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2019-03-10 10:02:32 -0400

    adds moment support for date formatting

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
@@ -19,7 +20,7 @@ const ProjectDetails = (props) => {
           </div>
           <div className="card-action grey  lighten-4 grey-text">
             <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-            <div>September 3rd, 2019</div>
+            <div>{moment(project.createdAt.toDate().toString()).calendar()}</div>
           </div>
         </div>
       </div>
